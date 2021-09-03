bd7131efc47f93de854c5271d20bdc72c57f1cd5
basarat <basaratali@gmail.com>
2021-09-03 16:05:09 +1000

    start

--- src/index.ts
+++
@@ -1,9 +0,0 @@
-let alpha = 'john';
-let beta = 'jane';
-
-// Don't use any new variables
-let one, two;
-[one, two] = [alpha, beta];
-
-// Test
-console.log(alpha == 'jane' && beta == 'john'); // true
